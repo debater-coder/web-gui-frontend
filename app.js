477b12720ddc7d0cecd66527673427c4b9372f9f
debater-coder <52619668+debater-coder@users.noreply.github.com>
2020-07-16 17:18:43 +1000

    fix bug

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const port = 3000
 const helmet = require('helmet')
 app.use(helmet())
 
-app.get('/', (req, res) => res.render('index.html'))
+app.get('/', (req, res) => res.sendfile('index'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
