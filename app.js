9f0def62aa5018d3c6d1322366db4bca618bcd24
debater-coder <52619668+debater-coder@users.noreply.github.com>
2020-07-16 16:51:48 +1000

    added camera feed

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const helmet = require('helmet')
+app.use(helmet())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index.html'))
 
-app.listen(port, () => console.log(`Example app listenings at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
