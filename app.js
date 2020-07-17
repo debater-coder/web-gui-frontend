7180cdbfe5b4d433630c355d5a0e15ada5c063f0
debater-coder <52619668+debater-coder@users.noreply.github.com>
2020-07-17 15:14:16 +1000

    fix styles

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 3000
 const helmet = require('helmet')
 app.use(helmet())
+app.use(express.static('static'))
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: __dirname})
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
