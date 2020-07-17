31697da1238dc4f0fdea731e78749a6a1545ef67
debater-coder <52619668+debater-coder@users.noreply.github.com>
2020-07-17 14:20:40 +1000

    Changed deprecated sendfile() to sendFile()

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3000
 const helmet = require('helmet')
 app.use(helmet())
 
-app.get('/', (req, res) => res.sendfile('index.html'))
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname})
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
